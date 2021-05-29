5bd56197af26affab2c326dd6e7827a18e9ef3c3
shankki_dev <skatkadeofficial@gmail.com>
2021-05-29 16:14:37 +0530

    added method is userServiceImpl

---
+++ src/main/java/com/customer/investment/investmentapp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.customer.investment.investmentapp.repository;
+
+import com.customer.investment.investmentapp.entity.UserDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserDetails, Integer> {
+
+
+}
