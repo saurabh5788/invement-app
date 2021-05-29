7cafe7a1852d7d36eb5127e72e4b8a646a11f85b
imDarshan <84025488+imDarshan@users.noreply.github.com>
2021-05-29 15:32:25 +0530

    added entity InvestmentAccount

--- src/main/java/com/customer/investment/investmentapp/entity/InvestmentAccount.java
+++ src/main/java/com/customer/investment/investmentapp/entity/InvestmentAccount.java
@@ -7,13 +7,36 @@ import javax.persistence.Id;
 
 @Entity
 public class InvestmentAccount {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String account_number;
-	private double amount ;
-	//private User user;
-	
+	private double amount;
+
+	// private User user;
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAccount_number() {
+		return account_number;
+	}
+
+	public void setAccount_number(String account_number) {
+		this.account_number = account_number;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 
 }
