31e1026c68507435adf61dc2be81d1739c1deec4
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-29 16:58:45 +0530

    Corrected DMLs as per Entity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO user_details (id, login_id ,password, name) values (1,'saurabh.singh5788@gmail.com','saurabh','Saurabh Singh');
-INSERT INTO user_details (id, login_id ,password, name) values (2,'skatkadeofficial@gmail.com','shankar','Shankar');
+INSERT INTO user_details (user_id, login_id ,password, name) values (1,'saurabh.singh5788@gmail.com','saurabh','Saurabh Singh');
+INSERT INTO user_details (user_id, login_id ,password, name) values (2,'skatkadeofficial@gmail.com','shankar','Shankar');
