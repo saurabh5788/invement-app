27eaa1e9a57885237641e6626d229c2552f1706b
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-29 18:15:09 +0530

    Removed Error DTO

--- src/main/java/com/customer/investment/investmentapp/controller/OrderController.java
+++ src/main/java/com/customer/investment/investmentapp/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.customer.investment.investmentapp.controller;
 
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.customer.investment.investmentapp.common.CustomApplicationProperties;
 import com.customer.investment.investmentapp.dtos.OrderStockRequestDTO;
 import com.customer.investment.investmentapp.service.StockService;
 
@@ -26,16 +30,21 @@ import com.customer.investment.investmentapp.service.StockService;
 public class OrderController {
 	
 	private StockService stockService;
+	private Map<String, String> userProperties;
 
 	@Autowired
-	public OrderController(StockService stockService) {
+	public OrderController(StockService stockService, CustomApplicationProperties customApplicationProperties) {
 		this.stockService = stockService;
+		this.userProperties = customApplicationProperties.getUser();
 	}
 
-	@PostMapping(value = "/users/{userid}/{accountid}/orders", consumes = 
+	@PostMapping(value = "/users/{userid}/orders", consumes = 
 			MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Void> placeOrder(
+	public ResponseEntity<Void> placeOrder(@PathVariable("userid") Integer userId,
 			@Valid @RequestBody OrderStockRequestDTO orderStockRequestDTO) {
+		if (userId == null || userId < 1) {
+			return ResponseEntity.badRequest().build();
+		}
 		boolean orderPlacedStatus = stockService.placeStockOrder(orderStockRequestDTO);
 		if (orderPlacedStatus)
 			return ResponseEntity.ok().build();
