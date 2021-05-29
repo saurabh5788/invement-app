8279d61431076baae3e038eb981e6f6c6ab5c25b
shankki_dev <skatkadeofficial@gmail.com>
2021-05-29 14:35:38 +0530

    user controller

---
+++ src/main/java/com/customer/investment/investmentapp/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.customer.investment.investmentapp.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.print.attribute.standard.Media;
+
+@RestController
+@RequestMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+
+
+
+}
