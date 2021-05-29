fd039fa7345909ededa1f3b129555a776164b3c6
imDarshan <84025488+imDarshan@users.noreply.github.com>
2021-05-29 16:13:45 +0530

    added entity/Order.java

---
+++ src/main/java/com/customer/investment/investmentapp/entity/Order.java
@@ -0,0 +1,75 @@
+package com.customer.investment.investmentapp.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int orderId;
+	private String stockSymbol;
+	private int quantity;
+	private double stockPrice;
+	private LocalDate orderDate;
+
+	@ManyToOne
+	@JoinColumn(name = "investment_account_id")
+	private InvestmentAccount investmentAccount;
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getStockSymbol() {
+		return stockSymbol;
+	}
+
+	public void setStockSymbol(String stockSymbol) {
+		this.stockSymbol = stockSymbol;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getStockPrice() {
+		return stockPrice;
+	}
+
+	public void setStockPrice(double stockPrice) {
+		this.stockPrice = stockPrice;
+	}
+
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public InvestmentAccount getInvestmentAccount() {
+		return investmentAccount;
+	}
+
+	public void setInvestmentAccount(InvestmentAccount investmentAccount) {
+		this.investmentAccount = investmentAccount;
+	}
+
+}
