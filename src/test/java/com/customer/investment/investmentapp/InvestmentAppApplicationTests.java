973412e28f61f5c85f6c40a79d5aacd58a8261da
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-29 12:02:18 +0530

    first commit

---
+++ src/test/java/com/customer/investment/investmentapp/InvestmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.investment.investmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
